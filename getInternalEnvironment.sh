aa44c2f42ce2f54b606c140a91e42df8a4c69600
Tavas Lattimer <github@kd9nsc.com>
2024-10-22 14:21:07 -0500

    Update getInternalEnvironment.sh

--- getInternalEnvironment.sh
+++ getInternalEnvironment.sh
@@ -1,13 +1,8 @@
 TEMPHUM="$(./dht22.out)"
 IFS=' - ' read -r TEMP HUMIDITY <<< "$TEMPHUM"
 
-if [[ $TEMPHUM =~ "x_x" ]]; then
-  NTEMPHUM="$(./dht22.out)"
-  NIFS=' - ' read -r NTEMP NHUMIDITY <<< "$NTEMPHUM"
-
-  
-  asl-tts -n 1000 -t "Internal Temperature is ${NTEMP:0:4} degrees fahrenheit." -v "en_US-kathleen-low.onnx"
-  asl-tts -n 1000 -t "Internal humidity is ${NHUMIDITY:0:2} percent." -v "en_US-kathleen-low.onnx"
+if [[ $TEMPHUM == *"x_x"* ]]; then
+  asl-tts -n 1000 -t "Unable to read the temperature and humidity. Please try again in a minute" -v "en_US-kathleen-low.onnx"
 else
   asl-tts -n 1000 -t "Internal Temperature is ${TEMP:0:4} degrees fahrenheit." -v "en_US-kathleen-low.onnx"
   asl-tts -n 1000 -t "Internal humidity is ${HUMIDITY:0:2} percent." -v "en_US-kathleen-low.onnx"

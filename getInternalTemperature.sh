3911eeb25787450ee5abdd0291dba2c676126a13
Tavas Lattimer <github@kd9nsc.com>
2024-10-22 14:27:08 -0500

    Update getInternalTemperature.sh

--- getInternalTemperature.sh
+++ getInternalTemperature.sh
@@ -1,5 +1,8 @@
 TEMPHUM="$(./dht22.out)"
-
 IFS=' - ' read -r TEMP HUMIDITY <<< "$TEMPHUM"
 
-asl-tts -n 1000 -t "Internal Temperature is ${TEMP:0:4} degrees fahrenheit." -v "en_US-kathleen-low.onnx"
+if [[ $TEMPHUM == *"x_x"* ]]; then
+  asl-tts -n 1000 -t "Unable to read the temperature. Please try again in a minute" -v "en_US-kathleen-low.onnx"
+else
+  asl-tts -n 1000 -t "Internal Temperature is ${TEMP:0:4} degrees fahrenheit." -v "en_US-kathleen-low.onnx"
+fi

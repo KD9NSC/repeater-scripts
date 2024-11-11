bf76d3fd6d945c7a8784ac00b55ce2c6432deb51
Tavas Lattimer <github@kd9nsc.com>
2024-11-11 14:34:39 -0600

    Update polytts.sh

--- polytts.sh
+++ polytts.sh
@@ -9,4 +9,4 @@ export AWS_DEFAULT_REGION=
 
 aws polly synthesize-speech --output-format mp3 --voice-id Joanna --text "$textinput" $outputfile.mp3
 ffmpeg -i $filename.mp3 -ar 8000 -ac 1 -ab 64 -f mulaw $outputfile.pcm -map 0:0 -map 0:0 -y
-/usr/sbin/asterisk -rvx "rpt localplay 52318 /home/repeater/$outputfile"
+/usr/sbin/asterisk -rvx "rpt localplay 52318 $(pwd)/$outputfile"

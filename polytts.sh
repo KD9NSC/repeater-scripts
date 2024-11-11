de1625796b84d92ed7a034c0d515f2bcb468cc1a
Tavas Lattimer <github@kd9nsc.com>
2024-11-11 14:09:16 -0600

    Create polytts.sh

---
+++ polytts.sh
@@ -0,0 +1,12 @@
+# ./polytts.sh <inputfile with extension> <output file without extension>
+
+outputfile=$2
+textinput=$1
+
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_DEFAULT_REGION=
+
+aws polly synthesize-speech --output-format mp3 --voice-id Joanna --text "$textinput" $outputfile.mp3
+ffmpeg -i $filename.mp3 -ar 8000 -ac 1 -ab 64 -f mulaw $outputfile.pcm -map 0:0 -map 0:0 -y
+/usr/sbin/asterisk -rvx "rpt localplay 52318 /home/repeater/$outputfile"

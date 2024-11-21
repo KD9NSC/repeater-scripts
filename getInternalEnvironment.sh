4366d4dfaed3b102b0108d436186f00cd5551a43
Tavas Lattimer <github@kd9nsc.com>
2024-11-21 12:29:28 -0600

    Update getInternalEnvironment.sh

--- getInternalEnvironment.sh
+++ getInternalEnvironment.sh
@@ -1,9 +1,10 @@
-TEMPHUM="$(./dht22.out)"
-IFS=' - ' read -r TEMP HUMIDITY <<< "$TEMPHUM"
+#! /bin/bash
 
-if [[ $TEMPHUM == *"x_x"* ]]; then
-  asl-tts -n 1000 -t "Unable to read the temperature and humidity. Please try again in a minute" -v "en_US-kathleen-low.onnx"
-else
-  asl-tts -n 1000 -t "Internal Temperature is ${TEMP:0:4} degrees fahrenheit." -v "en_US-kathleen-low.onnx"
-  asl-tts -n 1000 -t "Internal humidity is ${HUMIDITY:0:2} percent." -v "en_US-kathleen-low.onnx"
-fi
+# Get and parse data from DHT22
+temphum=$(/etc/asterisk/scripts/dht22.out)
+IFS=' - ' read -r TEMP HUMIDITY <<< "$temphum"
+
+# Say output
+/usr/bin/asl-tts -n 1000 -t "Repeater cabinet internal temperature is ${TEMP:0:4} degrees fahrenheit." -v "en_US-kathleen-low.onnx"
+/usr/bin/sleep 4
+/usr/bin/asl-tts -n 1000 -t "Repeater cabinet internal humidity is ${HUMIDITY:0:2} percent." -v "en_US-kathleen-low.onnx"

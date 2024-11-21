b647636665790ad301b35d4304e7e0dba67cb04d
Tavas Lattimer <github@kd9nsc.com>
2024-11-21 12:29:42 -0600

    Update getInternalHumidity.sh

--- getInternalHumidity.sh
+++ getInternalHumidity.sh
@@ -1,8 +1,8 @@
-TEMPHUM="$(./dht22.out)"
-IFS=' - ' read -r TEMP HUMIDITY <<< "$TEMPHUM"
+#! /bin/bash
 
-if [[ $TEMPHUM == *"x_x"* ]]; then
-  asl-tts -n 1000 -t "Unable to read the humidity. Please try again in a minute" -v "en_US-kathleen-low.onnx"
-else
-  asl-tts -n 1000 -t "Internal humidity is ${HUMIDITY:0:2} percent." -v "en_US-kathleen-low.onnx"
-fi
+# Get and parse data from DHT22
+temphum=$(/etc/asterisk/scripts/dht22.out)
+IFS=' - ' read -r TEMP HUMIDITY <<< "$temphum"
+
+# Say output
+/usr/bin/asl-tts -n 1000 -t "Repeater cabinet internal humidity is ${HUMIDITY:0:2} percent." -v "en_US-kathleen-low.onnx"
